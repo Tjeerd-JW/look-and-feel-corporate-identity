96cb86672037d74833a1cd681689d9f3bf36e7a2
Tjeerd <tj.wokke@gmail.com>
2025-11-25 13:04:33 +0100

    Capture button countdown #4

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 const heart = document.getElementById("heart");
 const tomato = document.getElementById("tomato");
 const star = document.getElementById("star")
+const clock = document.getElementById("countdown");
+
+
+window.onload = () => {
+    if (document.getElementById("countdown")) {
+        timer();
+    }
+}
 
 heart.addEventListener("click", () => {
     console.log("heart")
@@ -19,3 +27,31 @@ star.addEventListener("click", () => {
     var element = document.getElementById("star-sparkle");
     element.classList.add("sparkle");
 })
+
+function timer() {
+    console.log("coutdown started")
+    let seconds = Math.floor(Math.random() * (21600 - 3600) + 3600);
+    setInterval(() => {
+        seconds--;
+        clock.textContent = timeFormat(seconds);
+    }, 1000);
+
+    if (seconds <= 0) {
+        clock.textContent = "je was op niks aan het wachten"
+    }
+}
+
+
+function timeFormat(seconds) {
+    // uitrekenen hoeveel uren minuten en seconden er moeten zijn
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const secs = seconds % 60;
+
+    // zorgen dat er een 0 voorkomt zodat 1:3:2 -> 01:03:02 word
+    const h = String(hours).padStart(2, "0");
+    const m = String(minutes).padStart(2, "0");
+    const s = String(secs).padStart(2, "0");
+
+    return `${h}:${m}:${s}`
+}
